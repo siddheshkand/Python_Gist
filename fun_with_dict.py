e353b473045c7e8753b775b73b4bcff72020f54d
siddkand <siddkand@gmail.com>
2019-01-21 00:42:17 +0530

    multiple keys sort

--- fun_with_dict.py
+++ fun_with_dict.py
@@ -1,6 +1,6 @@
 di = {
     "sid": 3,
-    "sud": 1,
+    "jack": 1,
     "ram": 5,
 }
 
@@ -10,4 +10,22 @@ for a in di.items():
 for key, value in di.items():
     print(key + " " + str(value))
     print()
+
+
 # https://pythontips.com/2016/04/24/python-sorted-collections/
+
+
+# Sort dict by key
+def sort_dict_by_key(dictionary):
+    sorted_di = sorted(dictionary.items(), key=lambda t: t[0], reverse=True)
+    return sorted_di
+
+
+# Sort dict by values
+def sort_dict_by_values(dictionary):
+    sorted_di = sorted(dictionary.items(), key=lambda t: t[1], reverse=True)
+    return sorted_di
+
+
+print(sort_dict_by_key(di))
+print(sort_dict_by_values(di))
---
+++ multiple_key_sort_dict.py
@@ -0,0 +1,12 @@
+users = [
+    {'fname': 'Bucky', 'lname': 'Roberts'},
+    {'fname': 'Tom', 'lname': 'Roberts'},
+    {'fname': 'Bernie', 'lname': 'Zunks'},
+    {'fname': 'Jenna', 'lname': 'Hayes'},
+    {'fname': 'Johnny', 'lname': 'Concrete'},
+    {'fname': 'Donald', 'lname': 'Pump'},
+    {'fname': 'Ho-Lee', 'lname': 'Fuk'},
+    {'fname': 'Busta', 'lname': 'Nut'},
+    {'fname': 'Jedediah', 'lname': 'Springfield'},
+    {'fname': 'Super', 'lname': 'Mario'}
+]

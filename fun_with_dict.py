fe332d998756b03ff22505a20f83a8f43e2da4ff
siddkand <siddkand@gmail.com>
2019-01-21 00:01:35 +0530

    sorting in py with object

---
+++ fun_with_dict.py
@@ -0,0 +1,13 @@
+di = {
+    "sid": 3,
+    "sud": 1,
+    "ram": 5,
+}
+
+for a in di.items():
+    print(a)
+
+for key, value in di.items():
+    print(key + " " + str(value))
+    print()
+# https://pythontips.com/2016/04/24/python-sorted-collections/
